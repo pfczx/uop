4fe6e83384af3a274706b5e96d4a2e8b5e7a361e
pfczx <lemonen.092@gmail.com>
2026-07-09 18:29:54 +0200

    first

---
+++ src/main/java/com/platform/uop/UopApplication.java
@@ -0,0 +1,13 @@
+package com.platform.uop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platform/uop/UopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.uop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
