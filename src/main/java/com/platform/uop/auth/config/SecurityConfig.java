7c57adabd249e1c1a8b6bdc958504361921818c0
pfczx <lemonen.092@gmail.com>
2026-08-12 01:57:05 +0200

    added bean for security context repository

--- src/main/java/com/platform/uop/auth/config/SecurityConfig.java
+++ src/main/java/com/platform/uop/auth/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
@@ -51,6 +53,11 @@ public class SecurityConfig {
       AuthenticationConfiguration configuration) throws Exception {
 
     return configuration.getAuthenticationManager();
+  }
+
+  @Bean
+  public SecurityContextRepository securityContextRepository() {
+    return new HttpSessionSecurityContextRepository();
   }
 
 }
