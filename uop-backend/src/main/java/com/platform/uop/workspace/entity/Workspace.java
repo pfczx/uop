663c239dcaf548197350d56db8c343d95b077a12
pfczx <lemonen.092@gmail.com>
2026-09-23 18:04:55 +0200

    fix: workspace imports

--- uop-backend/src/main/java/com/platform/uop/workspace/entity/Workspace.java
+++ uop-backend/src/main/java/com/platform/uop/workspace/entity/Workspace.java
@@ -1,6 +1,15 @@
 package com.platform.uop.workspace.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
